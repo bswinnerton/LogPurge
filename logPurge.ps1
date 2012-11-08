02bc6bb90b73eefbb0ac6604d58dd1f1104a0191
Brooks Swinnerton <bswinnerton@gmail.com>
2012-11-08 12:13:03 -0500

    added instructions for scheduling

--- logPurge.ps1
+++ logPurge.ps1
@@ -2,6 +2,13 @@
 ## Manually execute this command before running this script
 #Set-ExecutionPolicy Unrestricted
 
+## To schedule this task using windows scheduler, use the following configurations:
+##   * Run whether user is logged in or not
+##   * Configure for Windows 7 / Server 2008 R2
+##   * Actions > Start a Program:
+##      - Program: powershell.exe
+##      - Arguments: "& 'E:\Scripts\logPurge.ps1'" (assuming this is the correct path)
+
 ## Define Variables
 
 # Number of days to keep logs
